1d2bde1996d9e16b843b19bf49e056407c66743f
Peter Bingham <petersbingham@hotmail.co.uk>
2026-02-18 16:08:03 +0000

    chore: enable authz middleware for unit tests (#51)

--- test/security/xss/system_controller_test.go
+++ test/security/xss/system_controller_test.go
@@ -44,6 +44,11 @@ func TestGetSystems_ForXSS(t *testing.T) {
 	ctx := cmkcontext.CreateTenantContext(t.Context(), tenant)
 	r := sql.NewRepository(db)
 
+	authClient := testutils.NewAuthClient(ctx, t, r, testutils.WithKeyAdminRole())
+
+	keyConfig := testutils.NewKeyConfig(func(_ *model.KeyConfiguration) {},
+		testutils.WithAuthClientDataKC(authClient))
+
 	systemWithProps := testutils.NewSystem(func(s *model.System) {
 		s.Properties = map[string]string{
 			"test": "a<SCRIPT></SCRIPT>b",
@@ -54,15 +59,17 @@ func TestGetSystems_ForXSS(t *testing.T) {
 		ctx,
 		t,
 		r,
+		keyConfig,
 		systemWithProps,
 	)
 
 	t.Run("Should show properties field on system with properties", func(t *testing.T) {
 		expected := &map[string]any{"test": "ab"}
 		w := testutils.MakeHTTPRequest(t, sv, testutils.RequestOptions{
-			Method:   http.MethodGet,
-			Endpoint: fmt.Sprintf("/systems/%s", systemWithProps.ID),
-			Tenant:   tenant,
+			Method:            http.MethodGet,
+			Endpoint:          fmt.Sprintf("/systems/%s", systemWithProps.ID),
+			Tenant:            tenant,
+			AdditionalContext: authClient.GetClientMap(),
 		})
 
 		assert.Equal(t, http.StatusOK, w.Code)
