3935230acf3e26ae616c128db99b10b73a3a9a5e
nnicora <75667951+nnicora@users.noreply.github.com>
2026-02-25 10:13:49 +0100

    feat: add noop plugins (#136)

---
+++ internal/plugins/builtin.go
@@ -0,0 +1,21 @@
+package plugins
+
+import (
+	"github.com/openkcm/plugin-sdk/pkg/catalog"
+
+	certificateissuernoop "github.com/openkcm/cmk/internal/plugins/certificate-issuer/noop"
+	identitymanagementnoop "github.com/openkcm/cmk/internal/plugins/identity-management/noop"
+	keymanagementnoop "github.com/openkcm/cmk/internal/plugins/key-management/noop"
+	keystoremanagementnoop "github.com/openkcm/cmk/internal/plugins/keystore-management/noop"
+	notificationnoop "github.com/openkcm/cmk/internal/plugins/notification/noop"
+	systeminformationnoop "github.com/openkcm/cmk/internal/plugins/system-information/noop"
+)
+
+func RegisterAllBuiltInPlugins(registry catalog.BuiltInPluginRegistry) {
+	identitymanagementnoop.Register(registry)
+	notificationnoop.Register(registry)
+	systeminformationnoop.Register(registry)
+	certificateissuernoop.Register(registry)
+	keystoremanagementnoop.Register(registry)
+	keymanagementnoop.Register(registry)
+}
