08dd7db7aeb9b0097c4b4080ef3ea0cba205328e
Joao Teixeira <joao.teixeira01@sap.com>
2026-02-27 17:32:05 +0000

    fix: down-migration

--- migrations/tenant/schema/0003_add_error_event_table.sql
+++ migrations/tenant/schema/0003_add_error_event_table.sql
@@ -3,5 +3,5 @@ ALTER TABLE events ADD error_message varchar(255);
 ALTER TABLE events ADD error_code varchar(255);
 
 -- +goose Down
-ALTER TABLE events DROP errorMessage;
-ALTER TABLE events DROP errorCode;
+ALTER TABLE events DROP error_message;
+ALTER TABLE events DROP error_code;
