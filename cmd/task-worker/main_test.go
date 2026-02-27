519163968691731f883bfaea48da3877f33cd7aa
João Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2026-02-27 16:44:23 +0000

    feat: Enable workflow for primary key switch (#126)

--- cmd/task-worker/main_test.go
+++ cmd/task-worker/main_test.go
@@ -51,6 +51,11 @@ func buildCfg(t *testing.T) *config.Config {
 		Certificates: config.Certificates{
 			ValidityDays: config.MinCertificateValidityDays,
 		},
+		Services: config.Services{
+			Registry: &commoncfg.GRPCClient{
+				Enabled: true,
+			},
+		},
 		Database: dbCfg,
 		Plugins:  testutils.SetupMockPlugins(testutils.SystemInfo),
 	}
