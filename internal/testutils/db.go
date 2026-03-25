5f29870fd3e44508b95af22de199be38662940b9
João Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2026-03-25 10:08:26 +0000

    chore: simplify key manager tests  (#177)
    Co-authored-by: nnicora <75667951+nnicora@users.noreply.github.com>

--- internal/testutils/db.go
+++ internal/testutils/db.go
@@ -180,6 +180,11 @@ func NewTestDB(tb testing.TB, cfg TestDBConfig, opts ...TestDBConfigOpt) (*multi
 
 	dbCon := newTestDBCon(tb, &cfg)
 
+	tb.Cleanup(func() {
+		sqlDB, _ := dbCon.DB.DB()
+		sqlDB.Close()
+	})
+
 	migrator, err := db.NewMigrator(sql.NewRepository(dbCon), &config.Config{Database: cfg.dbCon})
 	assert.NoError(tb, err)
 
@@ -420,11 +425,6 @@ func newTestDBCon(tb testing.TB, cfg *TestDBConfig) *multitenancy.DB {
 	)
 	assert.NoError(tb, err)
 
-	tb.Cleanup(func() {
-		sqlDB, _ := con.DB.DB()
-		sqlDB.Close()
-	})
-
 	return con
 }
 
@@ -440,6 +440,11 @@ func NewIsolatedDB(tb testing.TB, cfg config.Database) config.Database {
 		[]config.Database{},
 	)
 	assert.NoError(tb, err)
+
+	tb.Cleanup(func() {
+		sqlDB, _ := con.DB.DB()
+		sqlDB.Close()
+	})
 
 	name := processNameForDB(tb.Name())
 	assert.NoError(tb, err)
