841bda63ba2b2b8db502fba4a26adda9e429d84a
Joao Teixeira <joao.teixeira01@sap.com>
2026-04-29 14:35:14 +0100

    feat: async integration tests

--- cmd/task-scheduler/main.go
+++ cmd/task-scheduler/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
-	"log"
+	"errors"
+	"flag"
+	"fmt"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/openkcm/common-sdk/pkg/commoncfg"
 	"github.com/openkcm/common-sdk/pkg/logger"
@@ -14,30 +18,29 @@ import (
 	"github.com/openkcm/cmk/internal/async"
 	"github.com/openkcm/cmk/internal/config"
 	"github.com/openkcm/cmk/internal/constants"
+	"github.com/openkcm/cmk/internal/log"
 	cmklog "github.com/openkcm/cmk/internal/log"
+	statusserver "github.com/openkcm/cmk/utils/status_server"
+)
+
+var (
+	BuildInfo               = "{}"
+	gracefulShutdownSec     = flag.Int64("graceful-shutdown", 1, "graceful shutdown seconds")
+	gracefulShutdownMessage = flag.String("graceful-shutdown-message", "Graceful shutdown in %d seconds",
+		"graceful shutdown message")
+	ErrRegistryEnabled = errors.New("failed to create registry client")
 )
 
 const AppName = "scheduler"
 
-func start() error {
-	ctx, cancelOnSignal := signal.NotifyContext(
-		context.Background(),
-		os.Interrupt, syscall.SIGTERM,
-	)
-
-	defer cancelOnSignal()
-
-	cfg, err := config.LoadConfig(commoncfg.WithEnvOverride(constants.APIName + "_task_scheduler"))
-	if err != nil {
-		return oops.In("main").Wrapf(err, "failed to load the config")
-	}
-
-	// LoggerConfig initialisation
-	err = logger.InitAsDefault(cfg.Logger, cfg.Application)
+func run(ctx context.Context, cfg *config.Config) error {
+	err := logger.InitAsDefault(cfg.Logger, cfg.Application)
 	if err != nil {
 		return oops.In("main").
 			Wrapf(err, "Failed to initialise the logger")
 	}
+
+	statusserver.StartStatusServer(ctx, cfg)
 
 	cronJob, err := async.New(cfg)
 	if err != nil {
@@ -61,9 +64,45 @@ func start() error {
 	return nil
 }
 
-func main() {
-	err := start()
+// runFuncWithSignalHandling runs the given function with signal handling. When
+// a CTRL-C is received, the context will be cancelled on which the function can
+// act upon.
+// It returns the exitCode
+func runFuncWithSignalHandling(f func(context.Context, *config.Config) error) int {
+	ctx, cancelOnSignal := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt, syscall.SIGTERM,
+	)
+	defer cancelOnSignal()
+
+	cfg, err := config.LoadConfig(commoncfg.WithEnvOverride(constants.APIName + "_task_scheduler"))
 	if err != nil {
-		log.Fatal(err)
+		log.Error(ctx, "Failed to load the configuration", err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
+
+		return 1
 	}
+
+	log.Debug(ctx, "Starting the application", slog.Any("config", *cfg))
+
+	err = f(ctx, cfg)
+	if err != nil {
+		log.Error(ctx, "Failed to start the application", err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
+
+		return 1
+	}
+
+	// graceful shutdown so running goroutines may finish
+	_, _ = fmt.Fprintln(os.Stderr, fmt.Sprintf(*gracefulShutdownMessage, *gracefulShutdownSec))
+	time.Sleep(time.Duration(*gracefulShutdownSec) * time.Second)
+
+	return 0
+}
+
+func main() {
+	flag.Parse()
+
+	exitCode := runFuncWithSignalHandling(run)
+	os.Exit(exitCode)
 }
--- test/integration/async_test.go
+++ test/integration/async_test.go
@@ -2,6 +2,7 @@ package integration_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"strings"
 	"testing"
 	"time"
@@ -15,6 +16,7 @@ import (
 	"github.com/openkcm/cmk/internal/config"
 	"github.com/openkcm/cmk/internal/testutils"
 	integrationutils "github.com/openkcm/cmk/test/integration/integration_utils"
+	"github.com/openkcm/cmk/utils/ptr"
 )
 
 func TestAsync(t *testing.T) {
@@ -73,8 +75,98 @@ func TestAsync(t *testing.T) {
 	assert.Eventually(t, func() bool {
 		stdout.Reset()
 		stderr.Reset()
-		_, err = stdcopy.StdCopy(&stdout, &stderr, reader)
 		exitCode, reader, err = taskCli.Exec(t.Context(), []string{string(integrationutils.TaskCLI), "stats", "--queue", "default", "--queue-info"})
+		if err != nil {
+			return false
+		}
+		_, err = stdcopy.StdCopy(&stdout, &stderr, reader)
+		if err != nil {
+			return false
+		}
 		return err == nil && exitCode == 0 && strings.Contains(stderr.String(), `"Processed": 1`)
 	}, 5*time.Second, 100*time.Millisecond)
+}
+
+// This test checks that children tasks are created for the ammount of tenants
+// If tenant count is 10, for 1 task there should be at least 11 tasks (original + one per tenant)
+func TestAsyncFanout(t *testing.T) {
+	nTenants := 10
+	_, _, dbCfg := testutils.NewTestDB(t, testutils.TestDBConfig{}, testutils.WithGenerateTenants(nTenants))
+
+	ctlg := plugincatalog.CreateBuiltInPluginRegistry()
+	integrationutils.RegisterNoopPlugins(ctlg)
+	_, pluginCfgs := testutils.NewTestPlugins(ctlg.Retrieve()...)
+
+	_, grpcClient := testutils.NewGRPCSuite(t)
+	defer grpcClient.Close()
+
+	cfg := &config.Config{
+		Certificates: config.Certificates{
+			ValidityDays: config.MinCertificateValidityDays,
+		},
+		Database: dbCfg,
+		Plugins:  pluginCfgs,
+		Services: config.Services{
+			Registry: &commoncfg.GRPCClient{
+				Enabled: true,
+				Address: grpcClient.Target(),
+				SecretRef: &commoncfg.SecretRef{
+					Type: commoncfg.InsecureSecretType,
+				},
+			},
+		},
+		Scheduler: config.Scheduler{
+			Tasks: []config.Task{
+				{
+					TaskType: config.TypeHYOKSync,
+					Enabled:  ptr.PointTo(true),
+					Cronspec: "@every 1s",
+					Retries:  ptr.PointTo(3),
+					TimeOut:  5 * time.Minute,
+					FanOutTask: &config.FanOutTask{
+						Enabled: true,
+						Retries: ptr.PointTo(0),
+						TimeOut: 5 * time.Minute,
+					},
+				},
+			},
+		},
+	}
+
+	testutils.StartRedis(t, &cfg.Scheduler)
+
+	_ = integrationutils.RunCMKService(t, integrationutils.ServiceConfig{
+		Service: integrationutils.TaskWorker,
+	}, cfg)
+	_ = integrationutils.RunCMKService(t, integrationutils.ServiceConfig{
+		Service: integrationutils.TaskScheduler,
+	}, cfg)
+	taskCli := integrationutils.RunCMKService(t, integrationutils.ServiceConfig{
+		Service: integrationutils.TaskCLI,
+		Args:    []string{"--sleep"},
+	}, cfg)
+
+	var stdout, stderr bytes.Buffer
+	assert.Eventually(t, func() bool {
+		stdout.Reset()
+		stderr.Reset()
+		exitCode, reader, err := taskCli.Exec(t.Context(), []string{string(integrationutils.TaskCLI), "stats", "--queue", "default", "--queue-info"})
+		if err != nil {
+			return false
+		}
+		_, err = stdcopy.StdCopy(&stdout, &stderr, reader)
+		if err != nil {
+			return false
+		}
+
+		// Extract the Processed value
+		var stats struct {
+			Processed int `json:"Processed"`
+		}
+		if err := json.Unmarshal(stderr.Bytes(), &stats); err != nil {
+			return false
+		}
+
+		return exitCode == 0 && stats.Processed > 0 && stats.Processed%nTenants != 0 && stats.Processed > nTenants+1
+	}, 15*time.Second, 100*time.Millisecond)
 }
