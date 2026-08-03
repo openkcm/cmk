91f941a59506bc58994b17b796ecebb20d567410
Minh Nghia Nguyen <minh-nghia.nguyen@sap.com>
2026-08-03 12:46:02 +0100

    feat: read supportedRegions from CMK keystore pool config
    Signed-off-by: Minh Nghia Nguyen <minh-nghia.nguyen@sap.com>

--- internal/clients/registry/systems/errors.go
+++ internal/clients/registry/systems/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrSystemsClientDoesNotExist           = errors.New("systems client does not exist")
+	ErrSystemsClientEmptyFilter            = errors.New("systems client requires at least one filter criterion")
 	ErrSystemsClientFailedGettingSystems   = errors.New("systems client failed to list systems")
 	ErrSystemsClientFailedMappingSystems   = errors.New("systems client failed to map systems")
 	ErrSystemsClientFailedUpdatingKeyClaim = errors.New("systems client failed to update keyclaim")
