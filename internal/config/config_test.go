b66033ad68e0d7eb2049ca01bc8c5c3a9ce6da47
Joao Teixeira <joao.teixeira01@sap.com>
2026-09-03 12:19:31 +0100

    feat: add configurable system limit
    Signed-off-by: Joao Teixeira <joao.teixeira01@sap.com>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -99,6 +99,71 @@ func TestValidateScheduler(t *testing.T) {
 	})
 }
 
+func TestValidateTenantLimits(t *testing.T) {
+	t.Run("Should successfully validate minimum", func(t *testing.T) {
+		tl := config.Tenant{SystemLimit: config.MinTenantLimitSystems}
+		assert.NoError(t, tl.Validate())
+	})
+
+	t.Run("Should fail validation for Systems below minimum", func(t *testing.T) {
+		tl := config.Tenant{SystemLimit: 0}
+		err := tl.Validate()
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, config.ErrTenantLimitsSystemsBelowMinimum)
+	})
+}
+
+func TestValidateConfig(t *testing.T) {
+	cryptoCerts := []config.CryptoCert{
+		{
+			Name: "crypto-1",
+			Subject: config.CryptoCertSubject{
+				CommonNamePrefix: "test_",
+			},
+			RootCA: "test",
+		},
+	}
+
+	bytes, err := yaml.Marshal(cryptoCerts)
+	assert.NoError(t, err)
+
+	t.Run("Should validate", func(t *testing.T) {
+		cfg := config.Config{
+			Certificates: config.Certificates{
+				ValidityDays: config.MinCertificateValidityDays,
+			},
+			CryptoLayer: config.CryptoLayer{CertX509Trusts: commoncfg.SourceRef{
+				Source: commoncfg.EmbeddedSourceValue,
+				Value:  string(bytes),
+			}},
+			Tenant: config.Tenant{
+				SystemLimit: config.MinTenantLimitSystems,
+			},
+		}
+
+		err = cfg.Validate()
+		assert.NoError(t, err)
+	})
+
+	t.Run("Should error on invalid tenant config", func(t *testing.T) {
+		cfg := config.Config{
+			Certificates: config.Certificates{
+				ValidityDays: config.MinCertificateValidityDays,
+			},
+			CryptoLayer: config.CryptoLayer{CertX509Trusts: commoncfg.SourceRef{
+				Source: commoncfg.EmbeddedSourceValue,
+				Value:  string(bytes),
+			}},
+			Tenant: config.Tenant{
+				SystemLimit: 0,
+			},
+		}
+
+		err = cfg.Validate()
+		assert.ErrorIs(t, err, config.ErrTenantLimitsSystemsBelowMinimum)
+	})
+}
+
 func TestValidateTenantManager(t *testing.T) {
 	mutator := testutils.NewMutator(func() config.TenantManager {
 		return config.TenantManager{
